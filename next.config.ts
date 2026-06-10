cb61e28bd15fb817e12be3018400ed903b6443e2
Phong <phong940253@gmail.com>
2026-06-10 20:18:13 +0700

    chore: add .next-prod to gitignore and tsconfig, add conditional distDir in next.config for production builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Nếu biến IS_BUILDING=true, đổi thư mục dist sang '.next-prod'
+  distDir: process.env.IS_BUILDING === "true" ? ".next-prod" : ".next",
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  // Các cấu hình khác của bạn giữ nguyên...
 };
 
 export default nextConfig;
